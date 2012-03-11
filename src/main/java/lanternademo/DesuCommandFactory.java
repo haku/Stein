ab60c199c04d54949e51cc28ee324c8de2a6d342
はく <haku@vaguehope.com>
2012-03-11 01:02:54 +0000

    Refactor nested classes into own files

---
+++ src/main/java/lanternademo/DesuCommandFactory.java
@@ -0,0 +1,15 @@
+package lanternademo;
+
+import org.apache.sshd.common.Factory;
+import org.apache.sshd.server.Command;
+
+public class DesuCommandFactory implements Factory<Command> {
+
+	public DesuCommandFactory () {}
+
+	@Override
+	public Command create () {
+		return new DesuCommand();
+	}
+
+}
