f489ed6bde94925e07d756d7b2d44c5b31282cf4
haku <git@haku.me>
2014-07-08 21:55:16 +0100

    use ThreadGroup to log exceptions.

--- src/main/java/com/vaguehope/stein/ConsoleCommandFactory.java
+++ src/main/java/com/vaguehope/stein/ConsoleCommandFactory.java
@@ -7,35 +7,55 @@ import java.util.concurrent.atomic.AtomicInteger;
 
 import org.apache.sshd.common.Factory;
 import org.apache.sshd.server.Command;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 public class ConsoleCommandFactory implements Factory<Command> {
 
 	private static final String THREAD_NAME_PREFIX = "ConsoleSch";
 	private static final int CLIENT_THREADS = 5;
 
-	private final ScheduledExecutorService schEx = Executors.newScheduledThreadPool(CLIENT_THREADS, new NamedThreadFactory(THREAD_NAME_PREFIX));
+	private final ScheduledExecutorService schEx;
+
+	public ConsoleCommandFactory () {
+		this.schEx = Executors.newScheduledThreadPool(CLIENT_THREADS,
+				new NamedThreadFactory(new LoggingThreadGroup(Thread.currentThread().getThreadGroup(), THREAD_NAME_PREFIX), THREAD_NAME_PREFIX));
+	}
 
 	@Override
 	public Command create () {
 		return new ConsoleCommand(this.schEx);
 	}
 
+	private static class LoggingThreadGroup extends ThreadGroup {
+
+		private static final Logger LOG = LoggerFactory.getLogger(ConsoleCommandFactory.LoggingThreadGroup.class);
+
+		public LoggingThreadGroup (final ThreadGroup parent, final String namePrefix) {
+			super(parent, "tg-" + namePrefix);
+		}
+
+		@Override
+		public void uncaughtException (final Thread t, final Throwable e) {
+			LOG.error("Thread died: " + t.toString(), e);
+		}
+
+	}
+
 	private static class NamedThreadFactory implements ThreadFactory {
 
 		private final AtomicInteger counter = new AtomicInteger(0);
+		private final ThreadGroup threadGroup;
 		private final String namePrefix;
 
-		public NamedThreadFactory (String namePrefix) {
+		public NamedThreadFactory (final ThreadGroup threadGroup, final String namePrefix) {
+			this.threadGroup = threadGroup;
 			this.namePrefix = namePrefix;
 		}
 
 		@Override
-		public Thread newThread (Runnable r) {
-			SecurityManager s = System.getSecurityManager();
-			Thread t = new Thread(
-					s != null ? s.getThreadGroup() : Thread.currentThread().getThreadGroup(),
-					r,
-					this.namePrefix + this.counter.getAndIncrement());
+		public Thread newThread (final Runnable r) {
+			Thread t = new Thread(this.threadGroup, r, this.namePrefix + this.counter.getAndIncrement());
 			t.setDaemon(true);
 			t.setPriority(Thread.NORM_PRIORITY);
 			return t;
