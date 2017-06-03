a8358df527a23a4001ba88d37079c901f6654669
haku <git@haku.me>
2017-06-03 13:38:53 +0100

    Show POC dialog.

--- src/main/java/com/vaguehope/stein/DesuTerm.java
+++ src/main/java/com/vaguehope/stein/DesuTerm.java
@@ -2,14 +2,21 @@ package com.vaguehope.stein;
 
 import java.io.IOException;
 import java.util.Map.Entry;
+import java.util.concurrent.atomic.AtomicBoolean;
 import java.util.concurrent.atomic.AtomicInteger;
 
 import org.apache.sshd.server.Environment;
 import org.apache.sshd.server.ExitCallback;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 import com.googlecode.lanterna.SGR;
 import com.googlecode.lanterna.TerminalTextUtils;
 import com.googlecode.lanterna.graphics.TextGraphics;
+import com.googlecode.lanterna.gui2.MultiWindowTextGUI;
+import com.googlecode.lanterna.gui2.WindowBasedTextGUI;
+import com.googlecode.lanterna.gui2.dialogs.MessageDialog;
+import com.googlecode.lanterna.gui2.dialogs.MessageDialogButton;
 import com.googlecode.lanterna.input.KeyStroke;
 import com.googlecode.lanterna.input.KeyType;
 import com.googlecode.lanterna.screen.Screen;
@@ -17,23 +24,51 @@ import com.googlecode.lanterna.terminal.Terminal;
 
 public class DesuTerm extends SshConsole {
 
+	private static final Logger LOG = LoggerFactory.getLogger(DesuTerm.class);
+
 	private final AtomicInteger inputCounter = new AtomicInteger();
+	private WindowBasedTextGUI gui;
 
 	public DesuTerm (final String name, final Environment env, final Terminal terminal, final ExitCallback callback) throws IOException {
 		super(name, env, terminal, callback);
 	}
 
 	@Override
-	protected boolean readInput (final KeyStroke k) {
+	protected void initScreen (final Screen scr) {
+		scr.setCursorPosition(null);
+		this.gui = new MultiWindowTextGUI(scr);
+	}
+
+	@Override
+	protected boolean readInput (final KeyStroke k, final AtomicBoolean alive) throws InterruptedException {
 		if (k.getKeyType() == KeyType.Character) {
 			if (k.getCharacter() == 'q') {
-				scheduleQuit();
+				scheduleQuit("User pressed q.");
 				return false; // We are quitting.  Do not try and update UI.
+			}
+			else if (k.getCharacter() == 'd') {
+				LOG.info("Showing dialog...");
+				showDialog(this.gui, alive);
+				LOG.info("Dialog shown.");
+				return true;
 			}
 			this.inputCounter.incrementAndGet();
 			return true;
 		}
 		return false;
+	}
+
+	private void showDialog (final WindowBasedTextGUI gui, final AtomicBoolean alive) throws InterruptedException {
+		gui.getGUIThread().invokeAndWait(new Runnable() {
+			boolean run = false; // FIXME workaround for https://github.com/mabe02/lanterna/issues/310
+			@Override
+			public void run () {
+				if (this.run) return;
+				this.run = true;
+				final MessageDialogButton result = MessageDialog.showMessageDialog(gui, "foo", "desu", MessageDialogButton.OK, MessageDialogButton.Cancel);
+				LOG.info("Dialog result: {}", result);
+			}
+		});
 	}
 
 	@Override
